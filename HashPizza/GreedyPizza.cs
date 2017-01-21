aadf252f64720f07d0525e18bedc623eb52b0140
JPonte <joaopcponte@gmail.com>
2017-01-21 16:44:24 +0100

    Slice

---
+++ HashPizza/GreedyPizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPizza
+{
+    class GreedyPizza
+    {
+        GreedyPizza(Pizza pizza)
+        {
+        }
+    }
+}
---
+++ HashPizza/Slice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPizza
+{
+    class Slice
+    {
+        int left;
+        int right;
+        int top;
+        int bottom;
+
+        int getSize()
+        {
+            return (right - left) * (top - bottom);
+        }
+    }
+}
