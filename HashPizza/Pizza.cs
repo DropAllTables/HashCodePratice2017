611782a92add847d7f7aa781e6e3a0dd0cbd1865
Luís Reis <luiscubal@gmail.com>
2017-01-21 15:30:54 +0000

    Basic structure.

---
+++ HashPizza/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPizza
+{
+    class Pizza
+    {
+        public int MinIngredients;
+        public int MaxSize;
+        public Ingredient[,] Ingredients;
+    }
+}
